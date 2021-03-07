b94343b98e14a575ee764093c8125204fdc2b14f
nene26 <79775930+nene26@users.noreply.github.com>
2021-03-07 23:34:55 +0700

    Create ab.py

---
+++ ab.py
@@ -0,0 +1,10 @@
+import math
+
+def cal_rectangle_perimeter(a, b):
+    return 2*(a + b)
+
+def cal_circle_area(r):
+    return math.pi * pow(r, 2)
+
+def cal_rectangle_area(a, b):
+    return a * b
